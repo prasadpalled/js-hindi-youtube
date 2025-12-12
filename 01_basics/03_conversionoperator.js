c74499597a0934ef76d7985fec0c412ca2052ccf
prasadpalled <prasad062003@gmail.com>
2025-12-12 06:25:02 +0000

    conversion

---
+++ 01_basics/03_conversionoperator.js
@@ -0,0 +1,65 @@
+let score="23abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>NAN
+//true=>1
+//NAN=> its type is Number but it will not be number line 12
+
+let isLoggedIn="hitesh"
+
+let booleanisLoggedIn=Boolean(isLoggedIn)
+
+// console.log(typeof booleanisLoggedIn)
+// console.log(booleanisLoggedIn)
+
+//1=>true
+//""=>false
+//"prasad"=>true
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+//******************operations **************
+
+let value=3
+let negValue=-value
+
+//console.log(negValue)
+
+// console.log(2**2)
+
+let str1="prasad"
+let str2=" palled"
+
+let str3=str1+str2
+
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+// console.log(+true)
+// console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter)
