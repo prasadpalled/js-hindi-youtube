acffc92c92b0076f8dab82e83320a46456e5400d
prasadpalled <prasad062003@gmail.com>
2025-12-12 06:24:38 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3) we are using node not browser os it will not work but it will in browser
+console.log(3
+    +3) // code readability is important so dont write like this
+
+console.log("Prasad")
+
+let Uname="prasad";
+let age=23
+let isLoggedIn=true
+
+//numeber=>2^53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof Uname)
+console.log(typeof null)//object type
+console.log(typeof undefined) //undefined
