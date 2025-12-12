0cdc199e1e313e1d81510fd65e0e44a699a83952
prasadpalled <prasad062003@gmail.com>
2025-12-12 06:24:22 +0000

    Variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 122345
+let accountEmail = "prasad@gmail.com"
+var accountName="prasad"
+accountCity="jaipur"
+let accountState
+console.table([accountEmail,accountId,accountName,accountCity,accountState])
+/*
+prefer not use var it causes issue in bloack scope and functional scope
+*/
