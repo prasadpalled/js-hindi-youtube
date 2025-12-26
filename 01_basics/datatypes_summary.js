661af49617a75e5437ede8fe72d817d4737f413f
prasadpalled <prasad062003@gmail.com>
2025-12-26 06:16:28 +0000

    Stack and Heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -33,3 +33,33 @@ const myFunction = function() {
 }
 
 console.log(typeof myObj);
+
+
+
+
+
+//***************************************** */
+
+// Stack(Primitive) , Heap(Non-Primitive)
+
+let myYoutubename = "prasadpalled"
+
+let anothername = myYoutubename
+anothername = "prasad"
+
+console.log(anothername);
+console.log(myYoutubename);
+
+let user1 = {
+    email : "user@gmail.com",
+    age : 23
+}
+
+let user2 = user1
+
+user2.email = "user2@gamil.com"
+
+console.log(user1.email);
+console.log(user2.email);
+
+
